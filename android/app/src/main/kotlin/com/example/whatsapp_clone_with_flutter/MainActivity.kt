9b027790dafa899ee8393a576c37fb0a2cd65741
Nur <s.m.al-fuadnur@Ss-MacBook-Air.local>
2024-07-30 22:15:57 +0600

    Start coding...

---
+++ android/app/src/main/kotlin/com/example/whatsapp_clone_with_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.whatsapp_clone_with_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WhatsApp',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
