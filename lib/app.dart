df4ac1a4c1718fa54a50490e01b4a3972870f372
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-08-02 02:56:02 +0600

    Theme added

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:whatsapp_clone_with_flutter/features/splash/screens/splash.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'WhatsApp',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Scaffold(),
+      theme: ThemeData(brightness: Brightness.light),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      home: const Splash(),
     );
   }
 }
