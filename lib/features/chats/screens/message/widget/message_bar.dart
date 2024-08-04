abaa85ac16b12681135c309f94ff5612a755d2cf
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-08-04 16:51:27 +0600

    Message bar widget added

---
+++ lib/features/chats/screens/message/widget/message_bar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:whatsapp_clone_with_flutter/utils/constants/app_color.dart';
+
+class MessageBar extends StatelessWidget {
+  const MessageBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      child: Row(
+        children: [
+          Expanded(
+              child: Container(
+            decoration: BoxDecoration(
+                color: AppColor.messageBar,
+                borderRadius: BorderRadius.circular(30)),
+            child: Row(
+              children: [
+                IconButton(onPressed: () {}, icon: Icon(Icons.emoji_emotions_outlined)),
+                Flexible(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: 20.0,
+                      maxHeight: 135.0,
+                    ),
+                    child: Scrollbar(
+                      child: TextField(
+                        cursorColor: AppColor.white,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(vertical: 5),
+                          hintText: "Message",
+                          hintStyle: TextStyle(
+                            color:AppColor.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                IconButton(onPressed: () {}, icon: Icon(Icons.attach_file)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.camera_alt_outlined))
+              ],
+            ),
+          )),
+          SizedBox(
+            width: 5,
+          ),
+          IconButton(
+              style: IconButton.styleFrom(backgroundColor: AppColor.primary),
+              onPressed: () {},
+              icon: Icon(
+                Icons.mic,
+                color: AppColor.black,
+              ))
+        ],
+      ),
+    );
+  }
+}
