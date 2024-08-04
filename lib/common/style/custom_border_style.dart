711aa050f3980de2e0fe1626411a031cdd6ae027
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-08-04 16:49:15 +0600

    custom_border_style added

---
+++ lib/common/style/custom_border_style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone_with_flutter/utils/helpers/helper_function.dart';
+
+import '../../utils/constants/app_color.dart';
+
+class CustomBorderStyle{
+  static final dark = HelperFunctions.isDarkMode();
+  static BorderSide defaultBorderSideStyle = BorderSide(
+      color: dark
+          ? AppColor.defaultBorderDark
+          : AppColor.defaultBorderLight,
+      width: 1);
+
+  static Border defaultBorderStyle = Border.all(
+      color: dark
+          ? AppColor.defaultBorderDark
+          : AppColor.defaultBorderLight,
+      width: 1);
+}
