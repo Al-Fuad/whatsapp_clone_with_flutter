0f0c4a3a08ac81e85595abfbda62c5eeade5ad0b
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-08-04 16:35:39 +0600

    Constant color file added

---
+++ lib/utils/constants/app_color.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+
+  static Color primary = const Color(0xff25D366);
+  static Color secondaryDark = const Color(0xFF103629);
+  static Color secondaryLight = const Color(0xFFdcf8c6);
+
+  static Color white = Colors.white;
+  static Color black = Colors.black;
+  static Color grey = Colors.grey;
+
+  static Color messageBackgroundUser = const Color(0xFF103629);
+  static Color messageBackgroundFriend = Color(0xFF1F2C34);
+
+  static Color messageBar = Color(0xFF1F2C34);
+  static Color writeStatus = Color(0xFF1F2C34);
+
+  static Color backgroudLight = white;
+  static Color backgroudDark = const Color(0xFF0B141B);
+  static Color splashBackgroudLight = white;
+  static Color splashBackgroudDark = const Color(0xFF121B22);
+  static Color defaultBorderLight = const Color(0xFFF8F8F8);
+  static Color defaultBorderDark = const Color(0xFF101C26);
+
+}
