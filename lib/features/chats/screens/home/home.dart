26b287fbd0db7eada6cb31056dd92b9270f5e9fc
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-08-06 20:08:13 +0600

    .

--- lib/features/chats/screens/home/home.dart
+++ lib/features/chats/screens/home/home.dart
@@ -1,102 +1,96 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:whatsapp_clone_with_flutter/common/style/custom_border_style.dart';
+import 'package:whatsapp_clone_with_flutter/common/widget/appbar/action_list.dart';
 import 'package:whatsapp_clone_with_flutter/features/chats/screens/home/widget/archived.dart';
 import 'package:whatsapp_clone_with_flutter/features/chats/screens/home/widget/single_chat_tab.dart';
+import 'package:whatsapp_clone_with_flutter/features/personalization/screens/settings/settings.dart';
 import 'package:whatsapp_clone_with_flutter/utils/constants/app_color.dart';
 import 'package:whatsapp_clone_with_flutter/utils/helpers/helper_function.dart';
-
-import '../../../../bottom_nav.dart';
-import '../../../../common/widget/appbar/wa_app_bar.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    ActionList.cameraButtonPressed = () {};
+    ActionList.searchButtonPressed = () {};
+    ActionList.moreButtonText = ["New group","New broadcast","Linked devices","Starred messages","Settings"];
+    ActionList.moreButtonPressed = [Container(),Container(),Container(),Container(),const Settings()];
     final dark = HelperFunctions.isDarkMode();
     return Scaffold(
       backgroundColor: dark ? AppColor.backgroudDark : AppColor.backgroudLight,
       body: NestedScrollView(
         floatHeaderSlivers: true,
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) => [
-        SliverAppBar(
-          floating: true,
-          title: const Text("WhatsApp", style: TextStyle(fontWeight: FontWeight.w600),),
-          centerTitle: false,
-          backgroundColor: dark ? AppColor.backgroudDark : AppColor.backgroudLight,
-          shape: Border(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) =>
+            [
+          SliverAppBar(
+            floating: true,
+            title: const Text(
+              "WhatsApp",
+              style: TextStyle(fontWeight: FontWeight.w600),
+            ),
+            centerTitle: false,
+            backgroundColor:
+                dark ? AppColor.backgroudDark : AppColor.backgroudLight,
+            shape: Border(
               bottom: CustomBorderStyle.defaultBorderSideStyle,
+            ),
+            actions: ActionList.actions(),
           ),
-          actions: [
-            IconButton(
-              iconSize: 22,
-              icon: const Icon(
-                Icons.camera_alt_outlined,
-              ),
-              onPressed: (){},
-            ),
-            IconButton(
-              iconSize: 22,
-              icon: const Icon(Icons.search),
-              onPressed: (){},
-            ),
-            IconButton(
-              iconSize: 22,
-              icon: const Icon(Icons.more_vert),
-              onPressed: (){},
-            ),
-          ],
-        ),
-      ],body: SingleChildScrollView(
-        child: Column(
-          children: [
-            InkWell(
-              onTap: () => Get.to(() => const Archived()),
-              highlightColor: AppColor.secondaryDark,
-              splashFactory: NoSplash.splashFactory,
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                child: Row(
-                  children: [
-                    const Padding(padding: EdgeInsets.symmetric(horizontal: 7),child: Icon(Icons.archive_outlined)),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            "    Archived",
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          Text(
-                            "9",
-                            style: TextStyle(color: AppColor.primary),
-                          ),
-                        ],
+        ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              InkWell(
+                onTap: () => Get.to(() => const Archived()),
+                highlightColor: AppColor.secondaryDark,
+                splashFactory: NoSplash.splashFactory,
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  child: Row(
+                    children: [
+                      const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 7),
+                          child: Icon(Icons.archive_outlined)),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              "    Archived",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              "9",
+                              style: TextStyle(color: AppColor.primary),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SingleChatTab(),
-            const SingleChatTab(),
-            const SingleChatTab(),
-            const SingleChatTab(),
-            const SingleChatTab(),
-            const SingleChatTab(),
-            const SingleChatTab(),
-            const SingleChatTab(),
-            const SingleChatTab(),
-            const SingleChatTab(),
-            const SingleChatTab(),
-            const SingleChatTab(),
-            const SingleChatTab(),
-            const SingleChatTab(),
-            const SingleChatTab(),
-          ],
+              const SingleChatTab(),
+              const SingleChatTab(),
+              const SingleChatTab(),
+              const SingleChatTab(),
+              const SingleChatTab(),
+              const SingleChatTab(),
+              const SingleChatTab(),
+              const SingleChatTab(),
+              const SingleChatTab(),
+              const SingleChatTab(),
+              const SingleChatTab(),
+              const SingleChatTab(),
+              const SingleChatTab(),
+              const SingleChatTab(),
+              const SingleChatTab(),
+            ],
+          ),
         ),
-      ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
