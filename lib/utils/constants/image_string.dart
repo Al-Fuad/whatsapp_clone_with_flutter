75f5a84209344def12e96fc95b5d8b35aeea3277
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-08-08 00:49:39 +0600

    image_string.dart added

---
+++ lib/utils/constants/image_string.dart
@@ -0,0 +1,28 @@
+class ImageString {
+  static String backgroundImageDark =
+      "assets/backgrounds/backgroundImageDark.png";
+  static String backgroundImageLight =
+      "assets/backgrounds/backgroundImageLight.jpg";
+  static String backgroundCircle = "assets/backgrounds/backgroundCircle.png";
+
+  static String community1 = "assets/communities/community1.png";
+  static String community2 = "assets/communities/community2.png";
+  static String community3 = "assets/communities/community3.png";
+  static String community4 = "assets/communities/community4.png";
+
+  static String group1 = "assets/groups/group1.png";
+  static String group2 = "assets/groups/group2.png";
+  static String group3 = "assets/groups/group3.png";
+  static String group4 = "assets/groups/group4.png";
+
+  static String user1 = "assets/users/user1.png";
+  static String user2 = "assets/users/user2.png";
+  static String user3 = "assets/users/user3.png";
+  static String user4 = "assets/users/user4.png";
+
+  static String whatsapp = "assets/logos/whatsapp.png";
+  static String whatsappDark = "assets/logos/whatsapp_dark.png";
+  static String whatsappLogo = "assets/logos/whatsapp_logo.png";
+  static String meta = "assets/logos/meta.png";
+  static String metaDark = "assets/logos/meta_dark.png";
+}
