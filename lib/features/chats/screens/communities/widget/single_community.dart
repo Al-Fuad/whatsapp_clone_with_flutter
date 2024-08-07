3547f4398e81ae6dec5ffcab81b3cb3c1899e28c
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-08-07 23:28:19 +0600

    Screenshots for demo

---
+++ lib/features/chats/screens/communities/widget/single_community.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../common/style/custom_border_style.dart';
+import '../../../../../common/widget/image/circular_image.dart';
+import '../../../../../utils/constants/app_color.dart';
+import '../../../../../utils/helpers/helper_function.dart';
+
+class SingleCommunity extends StatelessWidget {
+  const SingleCommunity({
+    super.key,
+    required this.name,
+    required this.image,
+    required this.groupNames,
+    required this.groupImages,
+  });
+
+  final String name;
+  final String image;
+  final List<String> groupNames;
+  final List<String> groupImages;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = HelperFunctions.isDarkMode();
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            border: Border(bottom: CustomBorderStyle.defaultBorderSideStyle),
+            color: dark ? AppColor.backgroudDark : AppColor.backgroudLight,
+          ),
+          child: ListTile(
+            onTap: () {},
+            leading: CircularImage(
+              image: image,
+            ),
+            title: Text(name),
+          ),
+        ),
+        ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+          itemCount: groupNames.length,
+          itemBuilder: (BuildContext context, int index) => (index < 3) ? Container(
+            padding: EdgeInsets.all(10),
+            color: dark ? AppColor.backgroudDark : AppColor.backgroudLight,
+            child: ListTile(
+              onTap: () {},
+              leading: CircularImage(
+                image: groupImages[index],
+              ),
+              title: Text(groupNames[index]),
+            ),
+          ) : null,
+        ),
+        if(groupNames.length > 3)
+        Container(
+          height: 70,
+          padding: EdgeInsets.all(10),
+          color: dark ? AppColor.backgroudDark : AppColor.backgroudLight,
+          child: ListTile(
+            onTap: (){},
+            leading: Icon(Icons.keyboard_arrow_right),
+            title: Text("View All"),
+          ),
+        ),
+      ],
+    );
+  }
+}
